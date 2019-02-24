561d65f26a0158d10c54351ad989c5d8352b981f
Kodi Winterer <kodi.winterer.404@my.csun.edu>
2019-02-24 00:31:41 -0800

    Classes for: Class definitions, Member Declarations, Method Declarations
    and variables(in block of code rather than declared in a class as a
    member)

---
+++ src/JK_Lexer/ClassDefExp.java
@@ -0,0 +1,38 @@
+package JK_Lexer;
+
+import java.util.ArrayList;
+
+public class ClassDefExp {
+	public final Modifier mod; 
+	public final String name;
+	public final ArrayList<InstanceDecExp> members;
+	public final ArrayList<MethodDefExp> methods;
+	
+	public ClassDefExp(final Modifier mod,
+						final String name,
+						final ArrayList<InstanceDecExp> members,
+						final ArrayList<MethodDefExp> methods) {
+		this.mod=mod;
+		this.name=name;
+		this.members = new ArrayList<InstanceDecExp>(members);
+		this.methods = new ArrayList<MethodDefExp>(methods);
+	}
+	public int hashCode() {
+		return (mod.hashCode()+
+				name.hashCode()+
+				members.hashCode()+
+				methods.hashCode()); 
+	}
+	public boolean equals(final Object other) {
+		if(other instanceof ClassDefExp) {
+			final ClassDefExp otherExp=(ClassDefExp)other; 
+			return (otherExp.mod.equals(mod) &&
+					otherExp.name.equals(name) &&
+					otherExp.members.equals(members) &&
+					otherExp.methods.equals(methods));
+		}
+		else {
+			return false; 
+		}
+	}
+}
