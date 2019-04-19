a446303512eb75497a6059fcc74dba2ec02ce54e
Kodi Winterer <kodi.winterer.404@my.csun.edu>
2019-04-18 21:28:27 -0700

    Return and Assignment for C AST

---
+++ src/JK_Lexer/CAssignment.java
@@ -0,0 +1,30 @@
+package JK_Lexer;
+
+public class CAssignment {
+	public final CVariableExp v;
+	public final CExp e;
+	
+	public CAssignment(final CVariableExp v, CExp e) {
+		this.v=v;
+		this.e=e;
+	}
+	public int hashCode() {
+		return (v.hashCode()+
+				e.hashCode()); 
+	}
+	public boolean equals(final Object other) {
+		if(other instanceof CAssignment) {
+			final CAssignment otherExp=(CAssignment)other; 
+			return (otherExp.v.equals(v) &&
+					otherExp.e.equals(e));
+		}
+		else {
+			return false; 
+		}
+	}
+	
+	public String toString() {
+		String s = v.toString() + " = " + e.toString() +";";
+		return s;
+	}
+}
---
+++ src/JK_Lexer/CReturn.java
@@ -0,0 +1,31 @@
+package JK_Lexer;
+
+public class CReturn implements CStatement{
+public final Exp e;
+	
+	public CReturn(final Exp e) {
+		this.e=e;
+	}
+	
+	public CReturn() {
+		e = null;
+	}
+	
+	public int hashCode() {
+		return (e.hashCode()); 
+	}
+	public boolean equals(final Object other) {
+		if(other instanceof CReturn) {
+			final CReturn otherExp=(CReturn)other; 
+			return (otherExp.e.equals(e));
+		}
+		else {
+			return false; 
+		}
+	}
+	
+	public String toString() {
+		String s = "return " + e.toString() +";";
+		return s;
+	}
+}
